8f2159ea3acd24f6797e5fcfe21789068801a974
Juraj Hynek <juraj.hynek@gmail.com>
2021-10-12 00:33:49 +0100

    added dynacmi template to render array of dic

--- main.py
+++ main.py
@@ -9,29 +9,51 @@ def index():
     return render_template('home.temp.html')
 
 
-
 @app.route("/pacient-stories")
 def pacient_stories():
-     return render_template('pacient-stories.temp.html')
-    
+    pacient_stories = [{
+        "title": "Juraj story",
+        "subtitle": "Juraj damanges by doc nemeckova",
+        "path": "/juraj-story",
+        "image": "https://preview.redd.it/lrzvbgyuu8241.jpg?width=960&crop=smart&auto=webp&s=b2f8fc23b1f5fd733e82c312d35a3831a9f188cf"
+    }, {
+        "title": "Jana story",
+        "subtitle": "Juraj damanges by doc nemeckova",
+        "path": "/jana-story",
+          "image": "https://preview.redd.it/lrzvbgyuu8241.jpg?width=960&crop=smart&auto=webp&s=b2f8fc23b1f5fd733e82c312d35a3831a9f188cf"
+    }, {
+        "title": "Brona story",
+          "subtitle": "Juraj damanges by doc nemeckova",
+        "path": "/brona-story",
+          "image": "https://preview.redd.it/lrzvbgyuu8241.jpg?width=960&crop=smart&auto=webp&s=b2f8fc23b1f5fd733e82c312d35a3831a9f188cf"
+    }, {
+        "title": "Peter story",
+         "subtitle": "Juraj damanges by doc nemeckova",
+        "path": "peter-story",
+          "image": "https://preview.redd.it/lrzvbgyuu8241.jpg?width=960&crop=smart&auto=webp&s=b2f8fc23b1f5fd733e82c312d35a3831a9f188cf"
+    }, {
+        "title": "Katka story",
+         "subtitle": "Juraj damanges by doc nemeckova",
+        "path": "katka-story",
+          "image": "https://preview.redd.it/lrzvbgyuu8241.jpg?width=960&crop=smart&auto=webp&s=b2f8fc23b1f5fd733e82c312d35a3831a9f188cf"
+    }]
+
+    return render_template('pacient-stories.temp.html', data=pacient_stories)
+
 
 @app.route("/pacient-story")
 def pacient_story():
-     return render_template('pacient-story.temp.html')
-    
+    return render_template('pacient-story.temp.html')
 
 
 @app.route("/about-us")
 def about_us():
-      return render_template('about-us.temp.html')
-
+    return render_template('about-us.temp.html')
 
 
 @app.route("/about-doctors")
 def about_doctors():
-      return render_template('home.temp.html')
-
-
+    return render_template('home.temp.html')
 
 
 if __name__ == "__main__":
