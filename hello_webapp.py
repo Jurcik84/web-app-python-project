39935db7bad48f4df6c9ca611a40c1107ab003cb
Juraj Hynek <juraj.hynek@gmail.com>
2021-10-11 18:25:07 +0100

    uncomment idnex handler

--- hello_webapp.py
+++ hello_webapp.py
@@ -4,9 +4,9 @@ from flask import Flask, redirect, url_for, request, render_template
 app = Flask(__name__)
 
 
-# @app.route("/")
-# def index():
-#     return render_template('home.temp.html')
+@app.route("/")
+def index():
+    return render_template('home.temp.html')
 
 
 
